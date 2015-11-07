868214b0c82ee739000ea99a0c69c5e821eeba08
unknown <JonP@Price850G1.C2Media.local>
2015-11-07 14:17:56 -0500

    basic angular build completed

--- Sources/SPA.Web/Static/js/app.js
+++ Sources/SPA.Web/Static/js/app.js
@@ -1,6 +1,34 @@
 ﻿(function () {
     var app = angular.module('newsfeed', ['ngSanitize']);
 
+    app.directive('scroll', function () {
+        return {
+            restrict: 'A',
+            link: function ($scope, elem, attr, ctrl) {
+                $(window).bind('scroll', function () {
+                    var el = $("article[data-article='" + $scope.selectedArticle + "']");
+                    if ($(el).position() === undefined) return;
+
+                    var scrollTop = $(window).scrollTop();
+                    var articleTop = $(el).position().top;
+                    var articleHeight = $(el).outerHeight();
+
+                    $scope.readPercent = ((scrollTop - articleTop) / articleHeight) * 100;
+
+                    if (!$scope.clickScrolling) {
+                        if ($scope.readPercent > 100) {
+                            $scope.selectedArticle = $scope.selectedArticle + 1;
+                        } else if ($scope.readPercent < 0) {
+                            $scope.selectedArticle = $scope.selectedArticle - 1;
+                        }
+                    }
+
+                    $scope.$apply();
+                });
+            }
+        }
+    });
+
     app.controller('NewsFeedController', [
         '$scope', function ($scope) {
 
@@ -8,11 +36,12 @@
                 $scope.articles = [];
                 $scope.readPercent = 0;
                 $scope.selectedArticle = 0;
+                $scope.clickScrolling = false;
 
                 $scope.loadArticles();
             }
 
-            $scope.getReadPerecent = function () {
+            $scope.getReadPercent = function () {
                 return $scope.readPercent;
             }
 
@@ -24,14 +53,13 @@
             $scope.selectArticle = function (index) {
                 $scope.selectedArticle = index;
                 var el = $("article[data-article='" + index + "']");
-                $("body").animate({ scrollTop: el.offset().top + 1 }, "slow");
+                $("body").animate({ scrollTop: el.offset().top + 1 }, "slow").promise().done(function () { $scope.clickScrolling = false });
                 
             }
 
             $scope.loadArticles = function () {
                 // web api request here
                 $scope.articles = articles;
-                
             }
 
 
