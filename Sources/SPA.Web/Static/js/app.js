24a922d9b6c05daa6638e635e35209384b90cb73
unknown <JonP@Price850G1.C2Media.local>
2015-11-07 13:17:50 -0500

    add angular requires and setup basic app and controller

--- Sources/SPA.Web/Static/js/app.js
+++ Sources/SPA.Web/Static/js/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿(function () {
+    var app = angular.module('newsfeed', []);
+
+    app.controller('NewsFeedController' [
+        '$scope', function ($scope) {
+
+            var init = function () {
+
+            }
+
+
+            init();
+        }]);
+
+
+})
